4b9587d48cb9312b255f5244d24861d82a2f1605
Robert Schumann <robert.schumann@live.de>
2015-06-15 14:03:48 +0200

    smack löschen

--- APTANA-WORKSPACE/hello-router.js
+++
@@ -1,7 +0,0 @@
-var server = require('node-router').getServer();
-
-server.get("/", function (request, response) {
-	resonse.simpleText(200, "Hello World");
-});
-
-server.listen(8000, "localhost");
